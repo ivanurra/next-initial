1868ca8541d5bb61446a096264780482a3640879
Ivan Urra <ivan.urra.wd@gmail.com>
2022-03-09 08:37:02 +0100

    WIP

--- pages/about.js
+++ pages/about.js
@@ -2,18 +2,18 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home</title>
-        <meta name="description" content="Home Page" />
+        <title>About</title>
+        <meta name="description" content="About Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Go to <Link href="/about">About</Link>
+          Go to <Link href="/">Home</Link>
         </h1>
 
         <p className={styles.description}>
