a3e72e929040244917264dded31075e2c269f60e
Ivan Urra <ivan.urra.wd@gmail.com>
2022-03-19 08:36:40 +0100

    WIP

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,31 @@
 import { ActiveLink } from "./ActiveLink";
 import styles from "../components/Navbar.module.css";
 
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
+
 export const Navbar = () => {
   return (
     <nav className={styles.navbarlinks}>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="Contact" href="/contact" />
-      <ActiveLink text="About" href="/about" />
+      {menuItems.map((item) => (
+        <ActiveLink text={item.text} href={item.href} key={item.href} />
+      ))}
     </nav>
   );
 };
---
+++ pages/pricing/index.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { DarkLayout } from "../../components/layouts/DarkLayout";
+import { MainLayout } from "../../components/layouts/MainLayout";
+
+export default function Pricing() {
+  return (
+    <>
+      <h1>Pricing</h1>
+      <h1 className={"title"}>
+        Go to <Link href="/">Home</Link>
+      </h1>
+    </>
+  );
+}
+
+Pricing.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
